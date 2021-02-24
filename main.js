3735ab7f10ce8c6dcd6b846c5a33d412db7d96e7
Guilherme Amado <guiii@live.com>
2021-02-23 22:15:50 -0300

    Add path output to UI

--- main.js
+++ main.js
@@ -12,12 +12,19 @@ let aboutWindow;
 function createMainWindow() {
   mainWindow = new BrowserWindow({
     title: 'ImageShrink',
-    width: 500,
+    width: isDev ? 800 : 500,
     height: 600,
     icon: './assets/icons/Icon_256x256.png',
     resizable: isDev,
     backgroundColor: 'white',
+    webPreferences: {
+      nodeIntegration: true,
+    },
   });
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 
   mainWindow.loadFile('./app/index.html');
 }
